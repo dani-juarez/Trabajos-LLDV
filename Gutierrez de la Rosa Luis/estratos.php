3f9218606e234f2590b61ebbc85f692edea7177a
LUIS-GTR <agutierrezr06@gmail.com>
2018-12-30 11:48:27 -0600

    practicas1

---
+++ Gutierrez de la Rosa Luis/estratos.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="ese es el ejemplo de la clase 501">
+	<meta name="author" content="">
+	<link rel="stylesheet" href="css/bootstrap.min.css"
+	<tittle>501</tittle>
+</head>
+<body>
+	<main role="main">
+		<div class="jombotron">
+			<div class="container">
+				<h1 class="display-3"><center>ESTRATOS DE LA INGENIERIA WEBAPP</center></h1>
+			</div>
+		</div>
+	</main>
+	<h2><center>En un proceso tan rápido como es el proceso de Ingeniería Web, donde los tiempos de desarrollo y los ciclos de vida de los productos son tan cortos, ¿merece la pena el esfuerzo requerido por la gestión?dada su complejidad es imprescindible.
+
+      El desarrollo incluye una gran variedad de personal técnico y no técnico trabajando en paralelo,  el equipo de desarrollo debe dominar aspectos tan variopintos como, software basado en componentes, redes, diseño de arquitectura y navegación, diseño gráfico y de interfaces, lenguajes y estándares en Internet, test de aplicaciones Web, etc, lo que hace que el proceso de búsqueda y contratación de personal
+sea arduo.
+	</center></h2>
+	<h3><center>Definir el marco de trabajo
+    La efectividad de cualquier proceso de ingeniería depende de su adaptabilidad. Esto es, la organización del equipo de trabajo del proyecto, los modos de comunicación entre miembros del equipo, las actividades de ingeniería y las tareas que deben realizarse, la información que se recolecte y se cree, y los métodos empleados para producir un producto de alta calidad deben estar adaptados a la gente que realiza el trabajo, el plazo y las restricciones del proyecto, y al problema que se quiere resolver.
+
+Antes de definir un marco de trabajo de proceso para la IWeb se debe reconocer que:
+
+    Las WebApps con frecuencia se entregan de manera incremental.
+    Los cambios ocurrirán frecuentemente.
+    Los plazos son cortos.</center></h3>
+</body>
+</html>
